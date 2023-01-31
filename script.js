11ef281cc7afd7550a87d10554b19e6ec0fcd86f
Ana-Maria Zamrii <anamaria.zamrii@mail.utoronto.ca>
2023-01-31 13:35:10 -0500

    initialized like # & dislike # to 0; modified text on the transposition section of index.html

--- script.js
+++ script.js
@@ -15,17 +15,17 @@ submit.addEventListener("click", function(e){
     var note = document.getElementById("note1").value.toUpperCase();
     //check whether the inputed note is one of the 12 musical pitches in note_list
     if (note_list.includes(note)){
-        //if the inputed value is one of the 12 musical pitches in note_list at an index of 2 or greater, insert the note present at 2 indices prior into the html element with id "clarinet"
+        //if the inputed value is one of the 12 musical pitches in note_list at an index of 2 or greater, insert "On Bb Clarinet, this note sounds like: " the note present at 2 indices prior into the html element with id "clarinet"
         if (note_list.indexOf(note)>1) {
-            document.getElementById("clarinet").innerHTML=note_list[note_list.indexOf(note)-2]
+            document.getElementById("clarinet").innerHTML="On Bb Clarinet, this note sounds like: " + note_list[note_list.indexOf(note)-2].toString
         } 
-        //if the inputed value is the musical pitch in note_list at an index of 0, insert 'A#' into the html element with id "clarinet"
+        //if the inputed value is the musical pitch in note_list at an index of 0, insert "On Bb Clarinet, this note sounds like: A#"" into the html element with id "clarinet"
         else if (note_list.indexOf(note)===0) {
-            document.getElementById("clarinet").innerHTML=note_list[10]
+            document.getElementById("clarinet").innerHTML="On Bb Clarinet, this note sounds like: " + note_list[10].toString()
         } 
-        //if the inputed value is the 12 musical pitch in note_list at an index of 1, insert 'A' into the html element with id "clarinet"
+        //if the inputed value is the 12 musical pitch in note_list at an index of 1, insert "On Bb Clarinet, this note sounds like: A" into the html element with id "clarinet"
         else if (note_list.indexOf(note)===1) {
-            document.getElementById("clarinet").innerHTML=note_list[11]
+            document.getElementById("clarinet").innerHTML="On Bb Clarinet, this note sounds like: " + note_list[11].toString
         }
     } 
     //if the inputed note is not one of the 12 musical pitches in note_list, insert "please input a valid note from the list above" into the html element with id "clarinet"
@@ -38,6 +38,8 @@ submit.addEventListener("click", function(e){
 var like_btn = document.getElementById("like"); 
 //define a (changeable) variable equal to 0
 let like_no=0;
+//insert "0" into the html element with id "like_no"
+document.getElementById("like_no").innerHTML=like_no;
 //add an event listener which triggers a function when the like button on the "Home" page is clicked
 like_btn.addEventListener("click", function(){
     //add 1 to the like_no variable and insert the updated value into the html element with id "like_no"
@@ -49,6 +51,8 @@ like_btn.addEventListener("click", function(){
 var dislike_btn = document.getElementById("dislike"); 
 //define a (changeable) variable equal to 0
 let dislike_no=0;
+//insert "0" into the html element with id "dislike_no"
+document.getElementById("dislike_no").innerHTML=dislike_no;
 //add an event listener which triggers a function when the dislike button on the "Home" page is clicked
 dislike_btn.addEventListener("click", function(){
     //add 1 to the dislike_no variable and insert the updated value into the html element with id "dislike_no"
